971424c5a6c78b9b2ea8f889d6894ef0c47a6af6
john <coder.john.cheung@gmail.com>
2018-01-27 19:02:04 +0800

    IDE changes from Intelij IDEA to Android Studio 3.0

--- android/app/src/main/java/com/tcl/flutter/tv/MainActivity.java
+++ android/app/src/main/java/com/tcl/flutter/tv/MainActivity.java
@@ -1,53 +1,20 @@
 package com.tcl.flutter.tv;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
+
 import io.flutter.app.FlutterActivity;
-import io.flutter.plugin.common.BasicMessageChannel;
-import io.flutter.plugin.common.JSONMessageCodec;
 import io.flutter.plugins.GeneratedPluginRegistrant;
-import io.flutter.view.FlutterMain;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-
-import static android.content.ContentValues.TAG;
 
 public class MainActivity extends FlutterActivity {
 
-    private BasicMessageChannel messageChannel;
-
-    private String[] getArgsFromIntent(Intent intent) {
-        // Before adding more entries to this list, consider that arbitrary
-        // Android applications can generate intents with extra data and that
-        // there are many security-sensitive args in the binary.
-        ArrayList<String> args = new ArrayList<String>();
-        if (intent.getBooleanExtra("trace-startup", false)) {
-            args.add("--trace-startup");
-        }
-        if (intent.getBooleanExtra("start-paused", false)) {
-            args.add("--start-paused");
-        }
-        if (intent.getBooleanExtra("enable-dart-profiling", false)) {
-            args.add("--enable-dart-profiling");
-        }
-        if (!args.isEmpty()) {
-            String[] argsArray = new String[args.size()];
-            return args.toArray(argsArray);
-        }
-        return null;
-    }
+    private static final String TAG = MainActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         GeneratedPluginRegistrant.registerWith(this);
-        String[] args = getArgsFromIntent(getIntent());
-        FlutterMain.ensureInitializationComplete(getApplicationContext(), args);
-        messageChannel = new BasicMessageChannel<>(getFlutterView(), "flutter/keyevent", JSONMessageCodec.INSTANCE);
     }
 
     @Override
@@ -58,14 +25,8 @@ public class MainActivity extends FlutterActivity {
         }
         Log.d(TAG, "onKeyDown: ACTION_DOWN flags = " + event.getFlags()
                 + " codePoint = " + event.getUnicodeChar() + " keyCode = "
-                + event.getKeyCode() + " scanCode = " + event.getScanCode() + " metaState = " + event.getMetaState());
-        Map<String, Object> maps = new HashMap<>();
-        maps.put("flags", event.getFlags());
-        maps.put("codePoint", event.getUnicodeChar());
-        maps.put("keyCode", event.getKeyCode());
-        maps.put("scanCode", event.getScanCode());
-        maps.put("metaState", event.getMetaState());
-        //messageChannel.send(maps);
+                + event.getKeyCode() + " scanCode = " + event.getScanCode()
+                + " metaState = " + event.getMetaState());
         return super.onKeyDown(keyCode, event);
     }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -5,16 +5,17 @@ void main() {
   runApp(new MaterialApp(
     //showPerformanceOverlay: true,
     title: 'Flutter Study',
-    home: new RootWidget(),
+    home: new TvAppWidget(),
   ));
 }
 
-class RootWidget extends StatelessWidget {
+class TvAppWidget extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     final Size screenSize = MediaQuery.of(context).size;
     final double devicePixelRatio = MediaQuery.of(context).devicePixelRatio;
-    print('RootWidget screenSize = $screenSize devicePixelRatio = $devicePixelRatio');
+    print('TvAppWidget screenSize = $screenSize devicePixelRatio = $devicePixelRatio');
     return new PosterDemo(
       screenSize: screenSize,
     );
